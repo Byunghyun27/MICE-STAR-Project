82ec16fe929fde2b75ed5fb8a89fadcc4d74e0b9
Byunghyun Jeon <bh1227@naver.com>
2025-10-16 15:13:33 +0900

    2025.10.16 first commit

---
+++ src/main/java/com/mice_star/demo/MiceStarApplication.java
@@ -0,0 +1,13 @@
+package com.mice_star.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class MiceStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiceStarApplication.class, args);
+	}
+}
